72a59fe96b5d96cb24b08da398f06b15a31b8956
JuanDuran85 <duran85juan@gmail.com>
2019-12-05 11:53:09 -0300

    agregando JavaScript y Canvas Parte 2

--- js/script.js
+++ js/script.js
@@ -2,12 +2,36 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
 
-let pelota = new Pelota();
+let pelota = new Pelota(); //definiendo el objeto pelota
+
+//constructor de la pelota
+let Pelota = function () {
+    this.x = 200;
+    this.y = 200;
+    this.xVelocidad = -4;
+    this.yVelocidad = 6;
+};
+
+//construccion de la pelota
+let circulo = function (x,y,radio,llenarCirculo) {  
+    context.beginPath();
+    context.arc(x,y,radio,0,2*Math.PI,false); //se dibuja el circulo
+    if (llenarCirculo) {
+        context.fill();
+    }else{
+        context.stroke();
+    };
+};
+
+//metodo para dibujar
+Pelota.prototype.dibujo = function () {  
+    circulo(this.x, this.y, 10, thue);
+};
 
 setInterval(function(){  
-    context.clearRect(0, 0, 400, 400);
-    pelota.dibujo();
-    pelota.mover();
-    pelota.tocaBorde();
-    context.strokeRect(0, 0, 400, 400);
+    context.clearRect(0, 0, 400, 400); // se limpia el canvas
+    pelota.dibujo(); // se dibuja la pelota
+    pelota.mover(); // se realiza el movimiento
+    pelota.tocaBorde(); // rebote de pelota al tocar borde
+    context.strokeRect(0, 0, 400, 400); //se completa el proceso
 },30);
